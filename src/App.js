8d4b3c3334cd042a36b58ed24cf5598e0aaf6975
DHughes666 <graziemolto6@gmail.com>
2023-08-28 13:59:40 +0100

    Expense Tracker -Expanding components for tidy coding

--- src/App.js
+++ src/App.js
@@ -11,13 +11,13 @@ function App() {
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
+      title: 'Macbook Pro',
+      amount: 1264.07,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
+      title: 'Hosting subscription',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './ExpenseItem.css';
+import DateItem from "./DateItem";
 
 function ExpenseItem(props) {
-    const expenseDate = new Date().toLocaleDateString();
     
     return (
         <div className="expense-item">
-            <div>{props.date.toLocaleDateString()}</div>
+            <DateItem 
+                date = {props.date}
+            />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
