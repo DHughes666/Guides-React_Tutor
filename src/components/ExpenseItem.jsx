e709503a2422e9df7822b7abb8b3134fc4cedb7f
DHughes666 <graziemolto6@gmail.com>
2023-08-28 13:13:51 +0100

    Expense Tracker -Styling components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import './ExpenseItem.css';
 
 function ExpenseItem() {
     return (
-        <div>
+        <div className="expense-item">
             <div>August 28, 2023</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Car Insurance</h2>
-                <div>$ 294.67</div>
+                <div className="expense-item__price">$ 294.67</div>
             </div>
             
         </div>
