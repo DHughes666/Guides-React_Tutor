5a4392d852c2b30c23d624e70df1c4fe2b654c35
DHughes666 <graziemolto6@gmail.com>
2023-08-29 05:04:27 +0100

    Splitting components practice successful

---
+++ src/components/Expenses.jsx
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem"
+import "./Expenses.css";
+
+function Expenses(props) {
+    return (
+        <div className="expenses">
+            <ExpenseItem 
+                date = {props.expenses[0].date}
+                title = {props.expenses[0].title}
+                amount = {props.expenses[0].amount}
+            />
+            <ExpenseItem 
+                date = {props.expenses[1].date}
+                title = {props.expenses[1].title}
+                amount = {props.expenses[1].amount}
+            />
+            <ExpenseItem 
+                date = {props.expenses[2].date}
+                title = {props.expenses[2].title}
+                amount = {props.expenses[2].amount}
+            />
+            <ExpenseItem 
+                date = {props.expenses[3].date}
+                title = {props.expenses[3].title}
+                amount = {props.expenses[3].amount}
+            />
+        </div> 
+        
+    )
+}
+
+export default Expenses;
