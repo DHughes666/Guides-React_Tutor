5bc5204482cc3c9314be1628851fe7ad28d8c624
DHughes666 <graziemolto6@gmail.com>
2023-08-29 12:00:50 +0100

    Event handling initiated -useState

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -5,7 +5,7 @@ import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
-    let [title, setTitle] = useState(props.title)
+    const [title, setTitle] = useState(props.title);
 
     const handleClick = () => {
         setTitle("Changed");
