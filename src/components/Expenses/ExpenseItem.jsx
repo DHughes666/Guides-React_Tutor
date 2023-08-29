d1197af7bf4d8de873a567e313a7beba97542b57
DHughes666 <graziemolto6@gmail.com>
2023-08-29 11:38:23 +0100

    Event handling initiated

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseItem.css';
 import DateItem from "./DateItem";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+
+    let [title, setTitle] = useState(props.title)
+
+    const handleClick = () => {
+        setTitle("Changed");
+    };
     
     return (
         <Card className="expense-item">
             <DateItem date = {props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button onClick={handleClick}>Change Title</button>
         </Card>
-    )
+    );
 }
 
 export default ExpenseItem;
