008c195c7089de44ee63a3c03f252211159cc920
DHughes666 <graziemolto6@gmail.com>
2023-08-29 04:39:10 +0100

    Splitting components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -6,9 +6,7 @@ function ExpenseItem(props) {
     
     return (
         <div className="expense-item">
-            <DateItem 
-                date = {props.date}
-            />
+            <DateItem date = {props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
